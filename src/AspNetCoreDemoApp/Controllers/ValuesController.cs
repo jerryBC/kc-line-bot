61a73be1a28b473e34fe195b7ed376f996e2333d
jerryBC <dtbinhqb@gmail.com>
2017-10-28 14:17:36 +0700

    First commit

--- src/AspNetCoreDemoApp/Controllers/ValuesController.cs
+++ src/AspNetCoreDemoApp/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
+using AspNetCoreDemoApp.Models;
 
 namespace AspNetCoreDemoApp.Controllers
 {
@@ -31,6 +32,27 @@ namespace AspNetCoreDemoApp.Controllers
 				if (request.Content != null)
 				{
 					string temp = request.Content.ToString();
+
+					Event ev = new Event();
+
+					ev.Message = "{{\"id\": \"325708\",\"type\": \"text\",\"text\": \"Hello, world\"}}";
+					ev.ReplyToken = @"VC/sPEIz8O7WYo0THlaM5laorgw+GifJmN8cFR5eZ0seYxNsR3ZOVblQgOeI8xNaCOyTsUz2VsajbtAyt8hj7+NdP2/oYB+7eQ/FGnEAN/ICGCPj5nX36E848piCYi16BPkXlDR3N0CDiPnrfbpPcAdB04t89/1O/w1cDnyilFU=";
+
+
+					LineMessageHandler handler1 = new LineMessageHandler(ev);
+
+					if (ev != null)
+					{
+						return new HttpResponseMessage(System.Net.HttpStatusCode.Ambiguous);
+					}
+					else
+					{
+						return new HttpResponseMessage(System.Net.HttpStatusCode.BadGateway);
+					}
+
+					temp = (await request.Content.ReadAsStringAsync()).ToString();
+					await handler1.HandleTextMessage();
+
 					return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
 				}
 				else
